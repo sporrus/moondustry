4b5a1a6b75efe18862da0a737214e448a617238c
gorodmi <gorodmibot@gmail.com>
2022-08-09 22:34:21 +0300

    tbh

--- src/moondustry/Lua.java
+++ src/moondustry/Lua.java
@@ -1,9 +1,11 @@
 package moondustry;
 
 import arc.*;
+import arc.func.*;
 import arc.util.*;
 import mindustry.game.*;
 import org.luaj.vm2.*;
+import org.luaj.vm2.lib.*;
 import org.luaj.vm2.lib.jse.*;
 
 public class Lua {
@@ -12,5 +14,12 @@ public class Lua {
         globals.set("Events", CoerceJavaToLua.coerce(Events.class));
         globals.set("EventType", CoerceJavaToLua.coerce(EventType.class));
         globals.set("WorldLoadEvent", CoerceJavaToLua.coerce(EventType.WorldLoadEvent.class));
+        globals.set("cons", new LibFunction() {
+            @Override
+            public LuaValue call(LuaValue a) {
+                if (!a.isfunction()) return LuaValue.NIL;
+                return CoerceJavaToLua.coerce((Cons<Object>)(b) -> a.call(CoerceJavaToLua.coerce(b)));
+            }
+        });
     }
 }
