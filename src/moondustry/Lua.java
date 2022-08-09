cd590f08bf281f900cbd70b8a4e081547a76d576
gorodmi <gorodmibot@gmail.com>
2022-08-09 22:44:01 +0300

    tbh

--- src/moondustry/Lua.java
+++ src/moondustry/Lua.java
@@ -14,11 +14,66 @@ public class Lua {
         globals.set("Events", CoerceJavaToLua.coerce(Events.class));
         globals.set("EventType", CoerceJavaToLua.coerce(EventType.class));
         globals.set("WorldLoadEvent", CoerceJavaToLua.coerce(EventType.WorldLoadEvent.class));
+        globals.set("run", new LibFunction() {
+            @Override
+            public LuaValue call(LuaValue a) {
+                if (!a.isfunction()) return LuaValue.NIL;
+                return CoerceJavaToLua.coerce((Runnable) a::call);
+            }
+        });
+        globals.set("boolf", new LibFunction() {
+            @Override
+            public LuaValue call(LuaValue a) {
+                if (!a.isfunction()) return LuaValue.NIL;
+                return CoerceJavaToLua.coerce((Boolf<Object>)(b) ->
+                        (boolean) CoerceLuaToJava.coerce(a.call(CoerceJavaToLua.coerce(b)), boolean.class));
+            }
+        });
+        globals.set("boolp", new LibFunction() {
+            @Override
+            public LuaValue call(LuaValue a) {
+                if (!a.isfunction()) return LuaValue.NIL;
+                return CoerceJavaToLua.coerce((Boolp)() ->
+                        (boolean) CoerceLuaToJava.coerce(a.call(), boolean.class));
+            }
+        });
+        globals.set("floatf", new LibFunction() {
+            @Override
+            public LuaValue call(LuaValue a) {
+                if (!a.isfunction()) return LuaValue.NIL;
+                return CoerceJavaToLua.coerce((Floatf<Object>)(b) ->
+                        (float) CoerceLuaToJava.coerce(a.call(CoerceJavaToLua.coerce(b)), float.class));
+            }
+        });
+        globals.set("floatp", new LibFunction() {
+            @Override
+            public LuaValue call(LuaValue a) {
+                if (!a.isfunction()) return LuaValue.NIL;
+                return CoerceJavaToLua.coerce((Floatp)() ->
+                        (float) CoerceLuaToJava.coerce(a.call(), float.class));
+            }
+        });
         globals.set("cons", new LibFunction() {
             @Override
             public LuaValue call(LuaValue a) {
                 if (!a.isfunction()) return LuaValue.NIL;
                 return CoerceJavaToLua.coerce((Cons<Object>)(b) -> a.call(CoerceJavaToLua.coerce(b)));
+            }
+        });
+        globals.set("prov", new LibFunction() {
+            @Override
+            public LuaValue call(LuaValue a) {
+                if (!a.isfunction()) return LuaValue.NIL;
+                return CoerceJavaToLua.coerce((Prov<Object>)() ->
+                        CoerceLuaToJava.coerce(a.call(), Object.class));
+            }
+        });
+        globals.set("func", new LibFunction() {
+            @Override
+            public LuaValue call(LuaValue a) {
+                if (!a.isfunction()) return LuaValue.NIL;
+                return CoerceJavaToLua.coerce((Func<Object, Object>)(b) ->
+                        CoerceLuaToJava.coerce(a.call(CoerceJavaToLua.coerce(b)), Object.class));
             }
         });
     }
